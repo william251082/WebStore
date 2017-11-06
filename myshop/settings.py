0cc2c3f890c094f4b284e5e3230088510930f503
williamdelrosario <williamdelrosario@yahoo.com>
2017-11-06 22:56:43 +0100

    Built a shopping cart.

--- myshop/settings.py
+++ myshop/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop'
+    'shop',
+    'cart1'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart1'
