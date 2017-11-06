63d4d2cdbf5f25e9f35e022708c7dee9509eba29
williamdelrosario <williamdelrosario@yahoo.com>
2017-11-06 22:24:54 +0100

    Built a product catalog view page.

--- myshop/urls.py
+++ myshop/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^cart/', include('cart.urls', namespace='cart')),
+    #url(r'^orders/', include('orders.urls', namespace='orders')),
+    url(r'^', include('shop.urls', namespace='shop')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
